c32c6a156fbd9525c47bd0b7b42a78bd325c6c82
Mashylyamur <krabovna3@gmail.com>
2023-03-20 13:16:18 +0300

    some commit

--- module6_1.py
+++ module6_1.py
@@ -4,11 +4,14 @@ def get_thread(thread_name):
     time.sleep(1)
     print(f'Поток {thread_name}')
 s = ['one', 'two', 'three', 'four', 'five']
-#for i in s:
-#    get_thread(i)
+t1 = time.time()
+for i in s:
+    get_thread(i)
+print(f'Время последовательного выполнения: {time.time() - t1}')
 threads = [Thread(target = get_thread, args=(i, )) for i in s]
-
+t2 = time.time()
 for t in threads:
     t.start()
 for t in threads:
     t.join()
+print(f'Время параллельного выполнения: {time.time() - t2}')
--- module6_3.py
+++ module6_3.py
@@ -1,16 +1,19 @@
 import requests
 from threading import Thread
+import time
 
 def get_html(link):
-    sess = requests.get(urls)
+    sess = requests.get(link)
     print(sess.text)
-urls = ['https://www.google.ru/', 'https://www.youtube.com/', 'https://ru.wikipedia.org/', 'https://dzen.ru/', 'https://ria.ru/']
-#for i in urls:
-#    get_html(i)
-threads = [Thread(target = get_html, args=(i, )) for i in urls]
-
+links = ['https://www.google.ru/', 'https://www.youtube.com/', 'https://ru.wikipedia.org/', 'https://dzen.ru/', 'https://ria.ru/']
+t1 = time.time()
+for i in links:
+    get_html(i)
+print(f'Время последовательного выполнения: {time.time() - t1}')
+threads = [Thread(target = get_html, args=(i, )) for i in links]
+t2 = time.time()
 for t in threads:
     t.start()
 for t in threads:
     t.join()
-
+print(f'Время паралелльного выполнения: {time.time() - t2}')
